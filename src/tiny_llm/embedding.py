a55bf19be995c30786ba6a51ab99d69947046ab1
BlackGhostLzc <2427324519@qq.com>
2025-09-27 17:02:22 +0800

    qwen2-week1

--- src/tiny_llm/embedding.py
+++ src/tiny_llm/embedding.py
@@ -3,10 +3,18 @@ import mlx.core as mx
 
 class Embedding:
     def __init__(self, vocab_size: int, embedding_dim: int, weight: mx.array):
-        pass
+        if weight is None:
+            # 正态分布初始化
+            weight = mx.random.normal(shape=(vocab_size, embedding_dim)) * (1.0 / embedding_dim**0.5)
+        self.vocab_size = vocab_size
+        self.embedding_dim = embedding_dim
+        self.weight = weight  # shape: [vocab_size, embedding_dim]
+        
 
     def __call__(self, x: mx.array) -> mx.array:
-        pass
+        return self.weight[x]
+    
 
     def as_linear(self, x: mx.array) -> mx.array:
-        pass
+        return x @ self.weight.T
+
